650290c8af613a48e38818b21170a2f028270bcf
zilboe <1149845493@qq.com>
2024-11-16 16:46:29 +0800

    [any message]

--- src/main.c
+++ src/main.c
@@ -72,6 +72,9 @@ void scanner_process(const char *content, int content_len)
             case '-':
                 printf("MINUS - null\n");
             break;
+            case ';':
+                printf("SEMICOLON ; null\n");
+            break;
         }
     }
     printf("EOF  null\n");
