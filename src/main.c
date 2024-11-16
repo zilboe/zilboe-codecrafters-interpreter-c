ab4818f87700a3fa8c77c41a5dfdb0fd94efc655
zilboe <1149845493@qq.com>
2024-11-16 16:23:46 +0800

    [any message]

--- src/main.c
+++ src/main.c
@@ -17,17 +17,17 @@ int main(int argc, char *argv[]) {
     const char *command = argv[1];
 
     if (strcmp(command, "tokenize") == 0) {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        fprintf(stderr, "Logs from your program will appear here!\n");
+        // // You can use print statements as follows for debugging, they'll be visible when running tests.
+        // fprintf(stderr, "Logs from your program will appear here!\n");
         
         char *file_contents = read_file_contents(argv[2]);
 
         // Uncomment this block to pass the first stage
-        // if (strlen(file_contents) > 0) {
-        //     fprintf(stderr, "Scanner not implemented\n");
-        //     exit(1);
-        // } 
-        // printf("EOF  null\n"); // Placeholder, remove this line when implementing the scanner
+        if (strlen(file_contents) > 0) {
+            fprintf(stderr, "Scanner not implemented\n");
+            exit(1);
+        } 
+        printf("EOF  null\n"); // Placeholder, remove this line when implementing the scanner
         
         free(file_contents);
     } else {
